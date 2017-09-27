6ae559cae063944c5301d41977473ebdcfb3959e
Märt Konsap <mrt.konsap@gmail.com>
2017-09-27 20:56:54 +0300

    Moving backend to python so we can make use of chatterbot. At the moment using the Flask framework as it was very easy to setup, maybe will move to django if there is a benefit in doing so.

---
+++ backend/Waldur.py
@@ -0,0 +1,42 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+from chatterbot import ChatBot
+
+chatbot = ChatBot(
+    'asd',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    trainer='chatterbot.trainers.ChatterBotCorpusTrainer',
+    database='./chatterbotdb.sqlite3'
+)
+
+chatbot.train("chatterbot.corpus.english")
+
+
+app = Flask("Waldur")
+api = Api(app)
+
+
+parser = reqparse.RequestParser()
+parser.add_argument('query')
+
+
+class Query(Resource):
+    def post(self):
+        query = parser.parse_args()['query']
+        if query is not None:
+            response = chatbot.get_response(query)
+            return {'message': str(response)}
+        else:
+            return {'error': 'This url supports only POST with the argument query'}
+
+
+class Hello(Resource):
+    def get(self):
+        return {'message': 'Hello World!'}
+
+
+
+api.add_resource(Hello, '/hello')
+api.add_resource(Query, '/')
+
+app.run(port='4567')
