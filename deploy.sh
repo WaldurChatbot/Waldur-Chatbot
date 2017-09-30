2549248bc12fdb32917de5c0b7151fbd812fe9cf
Märt Konsap <mrt.konsap@gmail.com>
2017-09-30 16:33:31 +0300

    Script provider n+10

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 
-echo "ok"
-exit 0
+echo "Deploying"
 # if supplied argument is 'dev', use develop branch
 script = "./local_deploy.sh"
 if [ ! -z $1 ]; then
@@ -11,9 +10,7 @@ if [ ! -z $1 ]; then
     fi
 fi
 
-echo "ok"
 echo $script
-exit 0
 
 #openssl aes-256-cbc -K $encrypted_5899b1a8b456_key -iv $encrypted_5899b1a8b456_iv -in .travis/travis_deploy_key.enc -out travis_deploy_key -d
 #scp deploy.sh $DEVUSER@$DEVREMOTE:~/
