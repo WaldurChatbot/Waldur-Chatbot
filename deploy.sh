a96a35be70fd6c0ac76dd27b5f9d2293be26ae4b
Märt Konsap <mrt.konsap@gmail.com>
2017-09-30 16:30:02 +0300

    Script provider n+1

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,9 @@ if [ ! -z $1 ]; then
     fi
 fi
 
+echo "ok"
 echo $script
+exit 0
 
 #openssl aes-256-cbc -K $encrypted_5899b1a8b456_key -iv $encrypted_5899b1a8b456_iv -in .travis/travis_deploy_key.enc -out travis_deploy_key -d
 #scp deploy.sh $DEVUSER@$DEVREMOTE:~/
