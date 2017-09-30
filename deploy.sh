45c60c5c41b5fe7a71f73db50b2e0b5b874ae392
Märt Konsap <mrt.konsap@gmail.com>
2017-09-30 16:44:31 +0300

    Should work now 2

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,7 @@ fi
 
 echo $script
 
-openssl aes-256-cbc -K $encrypted_5899b1a8b456_key -iv $encrypted_5899b1a8b456_iv -in .travis/travis_deploy_key.enc -out travis_deploy_key -d
-scp deploy.sh $DEVUSER@$DEVREMOTE:~/
-ssh $DEVUSE@$DEVREMOTE $script
+openssl aes-256-cbc -K $encrypted_5899b1a8b456_key -iv $encrypted_5899b1a8b456_iv -in .travis/travis_deploy_key.enc -out .travis/travis_deploy_key -d
+chmod 600 .travis/travis_deploy_key
+scp -o "StrictHostKeyChecking no" -i .travis/travis_deploy_key deploy.sh $DEVUSER@$DEVREMOTE:~/
+ssh -o "StrictHostKeyChecking no" -i .travis/travis_deploy_key $DEVUSE@$DEVREMOTE './local_deploy.sh dev'
