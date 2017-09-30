7354e26d0ca24c60ae3755a094261f02b8da82bc
Märt Konsap <mrt.konsap@gmail.com>
2017-09-30 15:10:22 +0300

    Deployment script and encrypted ssh key for deploy

---
+++ deploy.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# the target should already have cloned this repo
+cd Waldur-Chatbot
+
+# if supplied argument is 'dev', use develop branch
+if [ ! -z $1 ]; then
+    if [ $1 == "dev" ]; then
+        git checkout develop
+    else
+        git checkout master
+    fi
+fi
+
+git pull
+
+# install requirements
+sudo pip install -r telegrambot/requirements.txt
+sudo pip install -r fleepbot/requirements.txt
+sudo pip install -r backend/requirements.txt
+
+# kill all python processes
+pkill python3.5
+
+# start processes
+cd backend
+nohup python3.5 Waldur.py > /dev/null 2>&1 &
+echo "Started backend"
+cd ../telegrambot
+nohup python3.5 telegrambot.py > /dev/null 2>&1 &
+echo "Started telegram bot"
+cd ../fleepbot
+nohup python3.5 fleepbot.py > /dev/null 2>&1 &
+echo "Started fleep bot"
+
+exit 0
