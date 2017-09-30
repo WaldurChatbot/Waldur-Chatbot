64412874e253d3f3d9aad84035a401a8a5274d5c
Märt Konsap <mrt.konsap@gmail.com>
2017-09-30 16:47:57 +0300

    Should work now 33

--- deploy.sh
+++ deploy.sh
@@ -2,16 +2,19 @@
 
 echo "Deploying"
 # if supplied argument is 'dev', use develop branch
-script="./local_deploy.sh"
+script="'./local_deploy.sh'"
 if [ ! -z $1 ]; then
     if [ $1 = "dev" ]; then
-        script="./local_deploy.sh dev"
+        script="'./local_deploy.sh dev'"
     fi
 fi
 
 echo $script
 
+echo "Decrypting private key"
 openssl aes-256-cbc -K $encrypted_5899b1a8b456_key -iv $encrypted_5899b1a8b456_iv -in .travis/travis_deploy_key.enc -out .travis/travis_deploy_key -d
 chmod 600 .travis/travis_deploy_key
-scp -o "StrictHostKeyChecking no" -i .travis/travis_deploy_key deploy.sh $DEVUSER@$DEVREMOTE:~/
-ssh -o "StrictHostKeyChecking no" -i .travis/travis_deploy_key $DEVUSE@$DEVREMOTE './local_deploy.sh dev'
+echo "Moving local_deploy to remote"
+scp -o "StrictHostKeyChecking no" -i .travis/travis_deploy_key local_deploy.sh $DEVUSER@$DEVREMOTE:~/
+echo "Executing local_deploy in remote"
+ssh -o "StrictHostKeyChecking no" -i .travis/travis_deploy_key $DEVUSE@$DEVREMOTE $script
