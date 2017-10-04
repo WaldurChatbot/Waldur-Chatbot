72e8af377a149d583e1ccaf9f9e6b03c4abfd3ab
Märt Konsap <mrt.konsap@gmail.com>
2017-10-04 21:33:09 +0300

    Fix deploy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -17,6 +17,6 @@ echo "Decrypting private key"
 openssl aes-256-cbc -K $encrypted_5899b1a8b456_key -iv $encrypted_5899b1a8b456_iv -in .travis/travis_deploy_key.enc -out .travis/travis_deploy_key -d
 chmod 600 .travis/travis_deploy_key
 echo "Moving local_deploy to remote"
-scp -o "StrictHostKeyChecking no" -i .travis/travis_deploy_key .travis/local_deploy.sh $USER@$REMOTE:~/
+scp -o "StrictHostKeyChecking no" -i .travis/travis_deploy_key .travis/local_deploy_backend.sh $USER@$REMOTE:~/
 echo "Executing local_deploy in remote"
 ssh -o "StrictHostKeyChecking no" -i .travis/travis_deploy_key $USER@$REMOTE $SCRIPT
