e8f2c8d16337c6b6b94307a4fb37c774aedd9c15
Märt Konsap <mrt.konsap@gmail.com>
2017-10-04 20:49:29 +0300

    Renamed local deploy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,12 +2,12 @@
 
 echo "Deploying"
 # if supplied argument is 'dev', use develop branch
-SCRIPT="./local_deploy.sh"
+SCRIPT="./local_deploy_backend.sh"
 USER=$PRODUSER
 REMOTE=$PRODREMOTE
 if [ ! -z $1 ]; then
     if [ $1 = "dev" ]; then
-        SCRIPT="./local_deploy.sh dev"
+        SCRIPT="./local_deploy_backend.sh dev"
         USER=$DEVUSER
         REMOTE=$DEVREMOTE
     fi
