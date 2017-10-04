c2fbea1ca8c55d0644aa9fa13606cff2c329f1d9
Märt Konsap <mrt.konsap@gmail.com>
2017-10-04 21:41:10 +0300

    Changed travis deploy rsa key

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -14,9 +14,9 @@ if [ ! -z $1 ]; then
 fi
 
 echo "Decrypting private key"
-openssl aes-256-cbc -K $encrypted_5899b1a8b456_key -iv $encrypted_5899b1a8b456_iv -in .travis/travis_deploy_key.enc -out .travis/travis_deploy_key -d
-chmod 600 .travis/travis_deploy_key
+openssl aes-256-cbc -K $encrypted_35a0e268d508_key -iv $encrypted_35a0e268d508_iv -in .travis/deploy_rsa.enc -out .travis/deploy_rsa -d
+chmod 600 .travis/deploy_rsa
 echo "Moving local_deploy to remote"
-scp -o "StrictHostKeyChecking no" -i .travis/travis_deploy_key .travis/local_deploy_backend.sh $USER@$REMOTE:~/
+scp -o "StrictHostKeyChecking no" -i .travis/deploy_rsa .travis/local_deploy_backend.sh $USER@$REMOTE:~/
 echo "Executing local_deploy in remote"
-ssh -o "StrictHostKeyChecking no" -i .travis/travis_deploy_key $USER@$REMOTE $SCRIPT
+ssh -o "StrictHostKeyChecking no" -i .travis/deploy_rsa $USER@$REMOTE $SCRIPT
