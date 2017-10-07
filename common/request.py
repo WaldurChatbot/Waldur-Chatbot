b268b27cbd5c9835226c79b541261939424eadc8
Märt Konsap <mrt.konsap@gmail.com>
2017-10-07 21:46:24 +0300

    It made no sense for common to have the url of the backend hardcoded

--- common/request.py
+++ common/request.py
@@ -1,36 +1,33 @@
-
 from requests import Session, Request
 import json
-import configparser
 import logging
-
-config = configparser.ConfigParser()
-config.read('../configuration.ini')
-backend = config['backend']
-backend_url = backend['url'] + ':' + backend['port']
 
 logger = logging.getLogger(__name__)
 
-session = Session()
 
+class Connection(object):
 
-def query(q):
-    request = Request(
-        'POST',
-        backend_url,
-        data=json.dumps({'query': q})
-    )
+    def __init__(self, url):
+        self.url = url
+        self.session = Session()
 
-    prepped = request.prepare()
-    prepped.headers['Content-Type'] = 'application/json'
-    logger.info("Sending request: " + str(request.data))
-    response = session.send(prepped)
+    def query(self, q):
+        request = Request(
+            'POST',
+            self.url,
+            data=json.dumps({'query': q})
+        )
 
-    response_json = response.json()
+        prepped = request.prepare()
+        prepped.headers['Content-Type'] = 'application/json'
+        logger.info("Sending request: " + str(request.data))
+        response = self.session.send(prepped)
 
-    if response.status_code == 200:
-        logger.info("Received response: " + response_json['message'])
-        return response_json
-    else:
-        logger.error("Received error response: " + response_json['message'])
-        raise Exception(response_json['message'])
+        response_json = response.json()
+
+        if response.status_code == 200:
+            logger.info("Received response: " + response_json['message'])
+            return response_json
+        else:
+            logger.error("Received error response: " + response_json['message'])
+            raise Exception(response_json['message'])
