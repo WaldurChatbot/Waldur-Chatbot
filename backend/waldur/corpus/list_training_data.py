4964211fa4f08ccd09dd6ffa1c8e8c419da653fa
Märt Konsap <mrt.konsap@gmail.com>
2017-10-18 18:44:02 +0300

    Implemented first training data and parsing data and generating data

---
+++ backend/waldur/corpus/list_training_data.py
@@ -0,0 +1,23 @@
+
+data = [
+    [
+        'Hello',
+        'What the fuck!'
+    ],
+    [
+        'Do you love me?',
+        'Yes'
+    ],
+    [
+        'asd',
+        'dsa'
+    ]
+]
+
+response = "REQUEST~GET~services"
+
+for i in ["procjects?", "give projcsts!", "GIVE ME MY PROJECTS!"]:
+    a = [i, response]
+    data.append(a)
+
+
