daf94659975b2025836d07f3166acd2d09f4aa5b
Märt Konsap <mrt.konsap@gmail.com>
2017-10-25 17:49:55 +0300

    Forgot a thing

--- backend/test/logic_requestlogicadapters_test.py
+++ backend/test/logic_requestlogicadapters_test.py
@@ -4,10 +4,7 @@ from unittest import TestCase, main, mock
 class TestRequestLogicAdapters(TestCase):
     def setUp(self):
         pass
-
-    def test(self):
-        self.fail("123")
-
+    
 
 if __name__ == '__main__':
     main()
