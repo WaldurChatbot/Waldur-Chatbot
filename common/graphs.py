e3aca748f63ac93d1f8f19ba7d11885af0ba688a
Daniel Kütt <danielkytt@hotmail.com>
2017-11-01 17:14:00 +0200

    graphs v0.1

--- common/graphs.py
+++ common/graphs.py
@@ -1,10 +1,52 @@
 import json
 import matplotlib.pyplot as plt
 import numpy as np
+import io
 
-class TotalCostsGraph():
 
-    ID = 0
-    NAME = 'graph_totalcosts'
+# todo we should make this like requests
 
-    # todo we should make this like requests
+def make_graph(data):
+    print("---------------------------------------")
+    print(data)
+    print("---------------------------------------")
+
+    plotx = data['x']
+    ploty = data['y']
+    print(plotx)
+    print(ploty)
+
+    N = len(ploty)
+
+    ind = np.arange(N)
+    width = 0.35
+    fig, ax = plt.subplots()
+
+    rects1 = ax.bar(ind, ploty, width, color='#2388d6')
+
+    ax.set_xlabel('Months')
+    ax.set_ylabel('Total costs')
+    ax.set_xticks(ind + width / 2)
+    ax.set_xticklabels(plotx)
+
+    autolabel(rects1, ax)
+
+    # plt.show()
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
+
+    return buf
+
+def autolabel(rects, ax):
+    # Get y-axis height to calculate label position from.
+    (y_bottom, y_top) = ax.get_ylim()
+    y_height = y_top - y_bottom
+
+    for rect in rects:
+        height = rect.get_height()
+        label_position = height + (y_height * 0.01)
+
+        ax.text(rect.get_x() + rect.get_width() / 2., label_position,
+                '%d' % int(height),
+                ha='center', va='bottom')
