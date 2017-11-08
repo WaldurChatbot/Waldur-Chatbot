4450e3c503d393ac766347cc731ecd5c1347d072
Daniel Kütt <danielkytt@hotmail.com>
2017-11-08 17:52:36 +0200

    made graphs a little bit more like requests, right now they're missing a common body

--- common/graphs.py
+++ common/graphs.py
@@ -4,22 +4,21 @@ import numpy as np
 import io
 
 
-# todo we should make this like requests
-
 def make_graph(data):
-    print("---------------------------------------")
-    print(data)
-    print("---------------------------------------")
+    if data['graphID'] == 1:
+        return totalCostsBarGraph(data)
+
+def totalCostsBarGraph(data):
 
     plotx = data['x']
     ploty = data['y']
-    print(plotx)
-    print(ploty)
 
     N = len(ploty)
 
     ind = np.arange(N)
     width = 0.35
+
+    plt.style.use('ggplot')
     fig, ax = plt.subplots()
 
     rects1 = ax.bar(ind, ploty, width, color='#2388d6')
@@ -32,14 +31,20 @@ def make_graph(data):
 
     autolabel(rects1, ax)
 
-    # plt.show()
     buf = io.BytesIO()
     plt.savefig(buf, format='png')
     buf.seek(0)
 
     return buf
 
+
 def autolabel(rects, ax):
+    """
+    Labels the bars of bar graph with the value of the bars
+    A helper function from the internet
+    :param rects: bars of the graph
+    :param ax: axes of the graph
+    """
     # Get y-axis height to calculate label position from.
     (y_bottom, y_top) = ax.get_ylim()
     y_height = y_top - y_bottom
