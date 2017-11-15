dc61271391012968551ac813f5a280c28e954dcb
Märt Konsap <mrt.konsap@gmail.com>
2017-11-15 18:39:32 +0200

    Fixd tests

--- common/test/test.py
+++ common/test/test.py
@@ -12,15 +12,15 @@ class MockResponse:
 
 
 def send_ok(request, *args, **kwargs):
-    return MockResponse({"data": "ok"}, 200)
+    return MockResponse([{"data": "ok"}], 200)
 
 
 def send_invalid_token(request, *args, **kwargs):
-    return MockResponse({"message": "Invalid token"}, 401)
+    return MockResponse([{"message": "Invalid token"}], 401)
 
 
 def send_error(request, *args, **kwargs):
-    return MockResponse({"message": "System error"}, 500)
+    return MockResponse([{"message": "System error"}], 500)
 
 
 def send_error_from_waldur_api(request, *args, **kwargs):
@@ -45,7 +45,7 @@ class BackendConnectionTests(TestCase):
 
     @mock.patch('requests.Session.send', side_effect=send_ok)
     def test_query_responds_with_with_dict_on_correct_request(self, mock_send):
-        response = self.conn.query("hello", "good_token")
+        response = self.conn.query("hello", "good_token")[0]
         self.assertDictEqual({"data": "ok"}, response)
 
     @mock.patch('requests.Session.send', side_effect=send_invalid_token)
@@ -69,7 +69,7 @@ class WaldurConnectionTests(TestCase):
 
     @mock.patch('requests.Session.send', side_effect=send_ok)
     def test_query_responds_with_with_dict_on_correct_request(self, mock_send):
-        response = self.conn.query("GET", {}, "test")
+        response = self.conn.query("GET", {}, "test")[0]
         self.assertDictEqual({"data": "ok"}, response)
 
     @mock.patch('requests.Session.send', side_effect=send_invalid_token)
